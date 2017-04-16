e3088d22cd85d3299996bc429dc86c6fb7f59f15
高语越 <1519035296@qq.com>
2017-04-16 15:31:04 +0800

    为mobile.js添加loading方法

--- 代码/src/main/webapp/app/js/mobile.utils.js
+++ 代码/src/main/webapp/app/js/mobile.utils.js
@@ -1,6 +1,39 @@
 /**
  * Created by arthurme on 2017/3/31.
  */
+
+/**
+ * 分页
+ * @param url url，不带 pageNumber和pageSize
+ * @param updateTable
+ * @param pageSize
+ * @constructor
+ */
+var pageNumber = 0;
+var Loading = function (url, updateTable, pageSize) {
+    "use strict";
+    $(document).ajaxStart(function () {
+        Pace.restart();
+    });
+    $.ajax({
+        url: url + "/pageNumber/" + pageNumber + "/pageSize/" + pageSize,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success:function (data) {
+            var nextPageNumber = data["nextPage"];
+            if(nextPageNumber <= pageNumber){
+                $("#loadingText").html("没有更多了");
+                return;
+            }
+            updateTable(data);
+            pageNumber = nextPageNumber;
+        },
+        error:function (XMLHttpRequest) {
+            
+        }
+    });
+};
+
 var Get = function (url,success) {
     "use strict";
 
