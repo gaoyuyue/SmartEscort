2627668936725e5ecb62881ac8c7884aeb7c7d58
王雍皓 <lielast@hotmail.com>
2017-07-10 18:13:08 +0800

    增加认证提醒，修复页面

--- 代码/src/main/java/cn/attackme/escort/Controller/Admin/VerifyManagementController.java
+++ 代码/src/main/java/cn/attackme/escort/Controller/Admin/VerifyManagementController.java
@@ -1,6 +1,11 @@
 package cn.attackme.escort.Controller.Admin;
 
 
+import cn.attackme.Wechat.Message.LinkMessage;
+import cn.attackme.Wechat.Message.RowMessage;
+import cn.attackme.Wechat.Message.TemplateMessage;
+import cn.attackme.Wechat.Util.MessageUtil;
+import cn.attackme.Wechat.Util.WechatProperties;
 import cn.attackme.escort.Model.Role;
 import cn.attackme.escort.Model.School;
 import cn.attackme.escort.Model.User;
@@ -16,6 +21,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2017/5/23.
@@ -57,10 +64,34 @@ public class VerifyManagementController {
             if (isPass) {
                 user.setAuthed(true);
                 userInfoService.save(user);
+                //发送消息给用户
+                String foreUrl = WechatProperties.authorizeUrl+"?appid="+
+                        WechatProperties.appid+
+                        "&redirect_uri="+
+                        WechatProperties.OAuth2Url+
+                        "&response_type=code&scope=snsapi_base&state=";
+                String backUrl = "#wechat_redirect";
+                TemplateMessage authoredMessage = new TemplateMessage();
+                authoredMessage.setTouser(user.getOpenid());
+                authoredMessage.setTemplate_id("QWNFfg_U6M45WKOEFTNGZpvQZmFz7JNK6C4YroMETo4");
+                authoredMessage.setUrl(foreUrl+"/User/UserInfomation/"+backUrl);
+                MessageUtil.postTemplate(authoredMessage);
                 return new ResponseEntity<Void>(HttpStatus.OK);
             }else {
                 user.setStuCardUrl(null);
                 userInfoService.save(user);
+                //发送消息给用户
+                String foreUrl = WechatProperties.authorizeUrl+"?appid="+
+                        WechatProperties.appid+
+                        "&redirect_uri="+
+                        WechatProperties.OAuth2Url+
+                        "&response_type=code&scope=snsapi_base&state=";
+                String backUrl = "#wechat_redirect";
+                TemplateMessage authoredMessage = new TemplateMessage();
+                authoredMessage.setTouser(user.getOpenid());
+                authoredMessage.setTemplate_id("w_wsVvI8hOFlnSYvetbiJZZ3NkM_BuI0RkUnIyL6XPs");
+                authoredMessage.setUrl(foreUrl+"/User/UserInfomation/"+backUrl);
+                MessageUtil.postTemplate(authoredMessage);
                 return new ResponseEntity<Void>(HttpStatus.OK);
             }
         }else {
